1139255d1318766844770fc1d4645fd1c9a644d9
Nicoll Oliver <nicolldon96@gmail.com>
2022-03-31 06:56:31 -0400

    flip logic and tested - works in single card com.

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Flashcard({ flashcard }) {
+
+  const [flip, setFlip] = useState(false)
+  
   return (
-    <div>
-      {flashcard.question}
+    <div onClick={() => setFlip(!flip)}>
+      {flip ? flashcard.answer : flashcard.question}
     </div>
   )
 }
