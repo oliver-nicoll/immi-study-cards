1ce95b6a5572fe1e8915ce155e29aacb2e54e3ed
Nicoll Oliver <nicolldon96@gmail.com>
2022-03-31 06:25:12 -0400

    add in fc and fcl components, plus just a static fake info for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+  const [flashcards, setFlashcards] = useState(sample_flashcards)
   return (
     <h1> Hello, let us study for your civics exam!</h1>
   );
 }
 
+  const sample_flashcards = [
+  {
+    id: 1,
+    question: 'Question one?',
+    answer: 'a',
+    options: [
+      'a',
+      'b',
+      'c',
+      'd'
+    ]
+  },
+  {
+    id: 2,
+    question: 'Question two?',
+    answer: 'd',
+    options: [
+      'a',
+      'b',
+      'c',
+      'd'
+    ]
+  }
+]
 export default App;
